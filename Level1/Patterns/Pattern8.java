68ee94b9a5540150ffdcbf022d631e3444061d1c
Tushar Verma <55954950+vtushar2@users.noreply.github.com>
2021-09-09 11:01:35 +0530

    Create Pattern8.java

---
+++ Level1/Patterns/Pattern8.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+
+  public static void main(String[] args) {
+    Scanner scn = new Scanner(System.in);
+
+    // write ur code here
+    int n=scn.nextInt();
+    int nsp=n-1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=nsp;j++)
+        {
+            System.out.print("\t");
+        }
+        System.out.println("*\t");
+        nsp--;
+    }
+
+  }
+}
